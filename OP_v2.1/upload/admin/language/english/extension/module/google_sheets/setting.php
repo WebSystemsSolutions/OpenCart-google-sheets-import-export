5097d0541aa70f7b88f6aa2a382875f778e6b313
kostya <gmkostya@gmail.com>
2021-12-06 09:13:43 +0200

    fix English language files

--- OP_v2.1/upload/admin/language/english/extension/module/google_sheets/setting.php
+++ OP_v2.1/upload/admin/language/english/extension/module/google_sheets/setting.php
@@ -4,52 +4,52 @@
 $_['heading_title'] = '<span style="color: #804B95;font-weight: bold;">google sheets import/export</span> (web-systems.solution)';
 
 // Text
-$_['text_success'] = 'Success: Настройки успешно обновлены!';
+$_['text_success'] = 'Success: Settings updated successfully!';
 
-$_['entry_language_export'] = 'Какой язык експортировать (после изменения сохранить)';
+$_['entry_language_export'] = 'Which language to export (save after changing)';
 
-$_['entry_save_danger'] = 'Перед изменением любого поля нужно сохранять настройки модуля';
+$_['entry_save_danger'] = 'Before changing any field, you need to save the module settings';
 
-$_['text_edit'] = 'Настройки';
-$_['text_tab_setting'] = 'Настройки';
-$_['text_tab_export'] = $_['entry_export'] = 'Експорт';
-$_['text_tab_import'] = $_['entry_import'] = 'Импорт';
-$_['text_tab_instruction'] = 'Инструкция';
-
-
-$_['instruction_step_1'] = 'Перейти на страницу апи на вкладку "Учетные данные"';
-$_['instruction_step_2'] = 'Кликнуть на "+ Создать учетные данные" и выбрать - Идентификатор клиента OAuth ';
-$_['instruction_step_3'] = 'Создать идентификатор клиента OAuth (выбрать Тип приложения - Другие типы )';
-$_['instruction_step_4'] = 'Скопировать " Идентификатор клиента (ClientId)" и " Секретный код клиента (ClientSecret)" в настройки модуля';
-$_['instruction_step_5'] = 'Скопировать в настройки модуля " sheet id " - это ИД Гугл таблицы';
-$_['instruction_step_6'] = 'Сохранить модуль';
-$_['instruction_step_7'] = 'Кликнуть на ссылку авторизации (Для работы через api google sheets нужно авторизоваться )';
-$_['instruction_step_8'] = 'Ввести код в новом появившемся окне';
-$_['instruction_step_9'] = 'Сохранить модуль';
-
-$_['entry_export_danger'] = 'Данные в таблице будут перезатерты';
-
-$_['entry_language_import'] = 'Какой язык импортировать (после изменения сохранить)';
-$_['entry_import_danger'] = 'Перед импортом убедитесь что поле для обновления заполнено, иначе будут создаватся новые товары';
-$_['entry_update_field'] = 'Обновлять товары по полю';
-
-$_['entry_attribute_group_import'] = 'В какую группу добавлять новые атрибуты';
+$_['text_edit'] = 'Settings';
+$_['text_tab_setting'] = 'Settings';
+$_['text_tab_export'] = $_['entry_export'] = 'Export';
+$_['text_tab_import'] = $_['entry_import'] = 'Import';
+$_['text_tab_instruction'] = 'Instruction';
 
 
-$_['text_login'] = 'Для работы через api google sheets нужно авторизоваться';
-$_['text_count_step'] = 'Количество експорта товаров за один шаг';
+$_['instruction_step_1'] = 'Go to the API page on the "Credentials" tab';
+$_['instruction_step_2'] = 'Click on "+ Create credentials" and select - OAuth Client ID  ';
+$_['instruction_step_3'] = 'Create OAuth client ID (select Application type - Other types) ';
+$_['instruction_step_4'] = 'Copy "ClientId" and "ClientSecret" to the module settings ';
+$_['instruction_step_5'] = 'Copy to the settings of the module "sheet id" - this is the Google ID of the table ';
+$_['instruction_step_6'] = 'Save the module ';
+$_['instruction_step_7'] = 'Click on the authorization link (To work through api google sheets you need to log in) ';
+$_['instruction_step_8'] = 'Enter the code in the new window that appears';
+$_['instruction_step_9'] = 'Save the module';
 
-$_['login_auth_text'] = 'Перейдите сначала по ссылке, авторизуйтесь, получите код, введите его сюда, сохраните, потом сохраните модуль';
+$_['entry_export_danger'] = 'The data in the table will be overwritten';
+
+$_['entry_language_import'] = 'Which language to import (save after change)';
+$_['entry_import_danger'] = 'Before importing, make sure that the update field is filled, otherwise new products will be created';
+$_['entry_update_field'] = 'Update products in the field';
+
+$_['entry_attribute_group_import'] = 'In which group to add new attributes';
+
+
+$_['text_login'] = 'To work through api google sheets you need to log in';
+$_['text_count_step'] = 'The number of exports of goods in one step';
+
+$_['login_auth_text'] = 'Follow the link first, log in, get the code, enter it here, save, then save the module';
 
 
 // Error
-$_['error_version_php'] = 'Для работы модуля нужна версия PHP 5.6+ ';
+$_['error_version_php'] = 'PHP version 5.6+ is required for the module to work';
 
-$_['error_language_field'] = 'Не заполена колонка языка!';
-$_['error_language_code'] = 'Не верный язык импорта!';
-$_['error_field_update'] = 'Не найдена колонка для обновления! - ';
-$_['error_document_title'] = 'Не найден заголовок документа';
+$_['error_language_field'] = 'The tongue column is not lit!';
+$_['error_language_code'] = 'Not the correct language of import!';
+$_['error_field_update'] = 'No update column found! -';
+$_['error_document_title'] = 'No document title found';
 
-$_['error_sheets_connect'] = 'Не верный ClientId, ClientSecret или sheet id!';
-$_['error_api_login'] = 'Не верный ClientId, ClientSecret или или sheet id!';
-$_['error_permission'] = 'У Вас нет прав на изменения модуля!';
+$_['error_sheets_connect'] = 'Not a valid ClientId, ClientSecret or sheet id!';
+$_['error_api_login'] = 'Not a valid Clientid, Clientsecret or shet id!';
+$_['error_permission'] = 'You do not have the right to change the module !!';
